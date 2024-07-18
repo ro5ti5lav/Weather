befb2122ceb0bbebb627d3d0fd7367e13068b9f0
Mikhail Kushnir <ro5ti5lav@yandex.ru>
2024-07-18 14:36:14 +0400

    init

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './HomePage';
+import WeatherForecast from './components/WeatherForecast';
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/weather/:city" element={<WeatherForecast />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
